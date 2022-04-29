c357cf90837d9cb0405b5b8dc56eb5995cfa6a3c
Gustavo Schuler <luis.schuler@grupoboticario.com.br>
2022-04-29 13:52:11 -0300

    fix: lint fixes

--- schemas/form.ts
+++ schemas/form.ts
@@ -1,7 +1,10 @@
 import * as Yup from 'yup';
 
 const formSchema = Yup.object().shape({
-    age: Yup.number().min(1, 'Invalid age').max(4, 'Invalid Age').required('Age is required'),
+    age: Yup.number()
+        .min(1, 'Invalid age')
+        .max(4, 'Invalid Age')
+        .required('Age is required'),
 });
 
 export { formSchema };
